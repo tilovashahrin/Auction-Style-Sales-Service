816acc7a6cfa1926e756f310245eb764bd037f52
rorychisholm <rchisholm@rogers.com>
2020-02-16 22:01:44 -0500

    Add files via upload

--- SQA_FrontEnd/SQA_FrontEnd/AccountManager.cpp
+++ SQA_FrontEnd/SQA_FrontEnd/AccountManager.cpp
@@ -4,14 +4,35 @@
 using namespace std;
 
 AccountManager::AccountManager() {
-		logStatus = false;
-		username = "";
+	logStatus = false;
+	username = "";
 } 
+
+AccountManager::AccountManager(bool _logStatus, char _user_type, string _username) {
+	logStatus = _logStatus;
+	user_type = _user_type;
+	username = _username;
+}
 	
 void AccountManager::login(string _username) {
-	//Check data base for username would go first 
-
-	username = _username;
-	logStatus = true;
+	if (logStatus) {
+		cout << username << " is already logged in.\n";
+	}
+	else {
+		//Check data base for username would go first 
+		username = _username;
+		logStatus = true;
+	}
+}
+void AccountManager::logout() {
+	if (!logStatus) {
+		cout << "User is already logged out.\n";
+	}
+	else {
+		username = "";
+		logStatus = false;
+		user_type = 0;
+		// needs to print transation file on logout
+	}
 }
 
