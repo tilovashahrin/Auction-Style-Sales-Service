786df5ea93b346cbb0eb9db6dd9cf9fa2448cc89
Tilova Shahrin <46762829+tilovashahrin@users.noreply.github.com>
2020-02-17 16:16:12 -0500

    Add files via upload

---
+++ SQA_FrontEnd/SQA_FrontEnd/Transaction.cpp
@@ -0,0 +1,30 @@
+#include "AccountManager.h"
+#include "UserActivity.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void Transaction::logout_trans() {
+
+}
+
+void Transaction::refund_trans() {
+
+}
+
+void Transaction::adv_trans() {
+
+}
+
+void  Transaction::bid_trans() {
+
+}
+
+void Transaction::account_file() {
+
+}
+
+void Transaction::items_file() {
+
+}
