9f6ec7439739ac6cc47116a74e4ea4c96f9d8112
rorychisholm <rchisholm@rogers.com>
2020-03-01 02:49:46 -0500

    Delete input_script.sh

--- input_script.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-for i in ./Test_Files/input/*
-do
-	filename=${i##*/};
-	echo "running test $i"
-		./SQA_FrontEnd/SQA_FrontEnd/a.out <$i> ./Test_Files/act_output/${filename%.*}.aop
-done 
