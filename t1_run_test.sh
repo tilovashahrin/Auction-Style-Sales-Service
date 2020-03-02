0df8545a67def3545aa6c7faa98da6d8e5c3da42
rorychisholm <rchisholm@rogers.com>
2020-03-02 01:21:28 -0500

    Add files via upload

--- t1_run_test.sh
+++ t1_run_test.sh
@@ -2,7 +2,7 @@
 for i in ./Test_Files/input/*.inp
 do
 	DB=./SQA_FrontEnd/SQA_FrontEnd/DataFiles; filename=${i##*/};
-	echo “running test ${filename%.*}”
+	echo "running test ${filename%.*}"
 	timeout 1 ./SQA_FrontEnd/SQA_FrontEnd/a.out $DB/UserDB.txt $DB/ItemDB.txt $DB/Transout.atf < $i > ./Test_Files/act_output/${filename%.*}.aop
 	if [ $? -eq 124 ]; then echo Test Timed Out; fi
 done
